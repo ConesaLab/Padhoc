04eebd96d8f449fcf6a9c6d0e281713d99b38e29
Salvacasani <salvacasani@gmail.com>
2020-04-20 23:04:10 +0200

    modified installation

--- install_packages.sh
+++ install_packages.sh
@@ -1,18 +1,16 @@
 #!/bin/sh
 
-apt-get update 
+apt-get update
 pip install argparse
-#pip install string
-apt-get install random
-apt-get install zeep
+pip install random
+pip install zeep
 pip install requests
 pip install libchebipy
 pip install random
 pip install multiprocessing
-pip isntall hashlib
+pip install hashlib
 pip install SOAPpy
 pip install socket
-pip install xml.socket
 pip install numpy
 pip install neo4j
 pip install sklearn
